6217b86afc25f00f1ba05512980ce697cf11b983
Suranjan Rai <Suranjanthulungrai@gmail.com>
2021-02-02 22:35:49 +0545

    scrolltotop

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import "popper.js/dist/popper.min";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import ScrollToTop from "./ScrollToTop";
 import Homepage from "./pages/homepage/homepage.components";
 import PageNotfound from "./pages/404/404.components";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Homepage} />
-      <Route component={PageNotfound} />
-    </Switch>
+    <>
+      <ScrollToTop>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route component={PageNotfound} />
+        </Switch>
+      </ScrollToTop>
+    </>
   );
 }
 
