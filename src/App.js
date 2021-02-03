db5238704f600344580e90a3ffb41558ffa0b569
Suranjan Rai <Suranjanthulungrai@gmail.com>
2021-02-03 20:48:14 +0545

    afain

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "popper.js/dist/popper.min";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import ScrollToTop from "./ScrollToTop";
 import Layouts from "./layouts/layout.components";
 import PageNotfound from "./pages/404/404.components";
 import Pokedex from "./pages/pokedex/pokedex.components";
@@ -13,13 +14,15 @@ import Homepage from "./pages/homepage/homepage.components";
 
 function App() {
   return (
-    <Layouts>
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/pokedex" component={Pokedex} />
-        <Route component={PageNotfound} />
-      </Switch>
-    </Layouts>
+    <ScrollToTop>
+      <Layouts>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/pokedex" component={Pokedex} />
+          <Route component={PageNotfound} />
+        </Switch>
+      </Layouts>
+    </ScrollToTop>
   );
 }
 
