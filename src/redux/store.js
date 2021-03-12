bc41380a4d8986359adeeb92ca43cf7674615f3b
Suranjan Rai <Suranjanthulungrai@gmail.com>
2021-03-12 22:46:24 +0545

    redux fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,13 @@ const middlewares = [thunk];
 
 export const store = createStore(
   rootReducer,
-  compose(applyMiddleware(...middlewares))
+  compose(
+    applyMiddleware(...middlewares),
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
+      ? (a) => a
+      : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
-export default store;
+export default { store };
