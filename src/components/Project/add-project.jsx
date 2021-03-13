1ef394398ae0dc8c4f35f0b64f0a130fe00a69ca
Suranjan Rai <Suranjanthulungrai@gmail.com>
2021-03-13 12:41:31 +0545

    error styling and handeling

--- src/components/Project/add-project.jsx
+++ src/components/Project/add-project.jsx
@@ -31,6 +31,7 @@ class AddProject extends Component {
 
   onChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
+    this.setState({ errors: {} });
   };
 
   onSubmit = (e) => {
@@ -48,74 +49,93 @@ class AddProject extends Component {
 
   render() {
     const { errors } = this.state;
-    return (
-      <div>
-        <div className="project">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-6 mb-5 mx-auto">
-                <h5 className="display-4 text-center">Create Project form</h5>
-                <hr />
-                <form onSubmit={this.onSubmit}>
-                  <div className="form-group">
-                    <input
-                      type="text"
-                      className="form-control form-control-lg "
-                      placeholder="Project Name"
-                      name="projectName"
-                      value={this.state.projectName}
-                      onChange={this.onChange}
-                    />
-                    <p>{errors.projectName}</p>
-                  </div>
-                  <div className="form-group">
-                    <input
-                      type="text"
-                      className="form-control form-control-lg"
-                      placeholder="Unique Project ID"
-                      name="projectIdentifier"
-                      value={this.state.projectIdentifier}
-                      onChange={this.onChange}
-                    />
-                    <p>{errors.projectIdentifier}</p>
-                  </div>
-                  <div className="form-group">
-                    <textarea
-                      className="form-control form-control-lg"
-                      placeholder="Project Description"
-                      name="description"
-                      value={this.state.description}
-                      onChange={this.onChange}
-                    />
-                    <p>{errors.description}</p>
-                  </div>
-                  <h6>Start Date</h6>
-                  <div className="form-group">
-                    <input
-                      type="date"
-                      className="form-control form-control-lg"
-                      name="start_date"
-                      value={this.state.start_date}
-                      onChange={this.onChange}
-                    />
-                  </div>
-                  <h6>Estimated End Date</h6>
-                  <div className="form-group">
-                    <input
-                      type="date"
-                      className="form-control form-control-lg"
-                      name="end_date"
-                      value={this.state.end_date}
-                      onChange={this.onChange}
-                    />
-                  </div>
 
+    return (
+      <div className="project">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 mb-5 mx-auto">
+              <h5 className="display-4 text-center">Create Project form</h5>
+              <hr />
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
                   <input
-                    type="submit"
-                    className="btn btn-primary btn-block mt-4"
+                    type="text"
+                    className={
+                      errors.projectName
+                        ? " form-control form-control-lg border-danger"
+                        : "form-control form-control-lg"
+                    }
+                    placeholder="Project Name"
+                    name="projectName"
+                    value={this.state.projectName}
+                    onChange={this.onChange}
                   />
-                </form>
-              </div>
+                  {errors.projectName && (
+                    <small className="text-danger">{errors.projectName}</small>
+                  )}
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className={
+                      errors.projectIdentifier
+                        ? " form-control form-control-lg border-danger"
+                        : "form-control form-control-lg"
+                    }
+                    placeholder="Unique Project ID"
+                    name="projectIdentifier"
+                    value={this.state.projectIdentifier}
+                    onChange={this.onChange}
+                  />
+                  {errors.projectIdentifier && (
+                    <small className="text-danger">
+                      {errors.projectIdentifier}
+                    </small>
+                  )}
+                </div>
+                <div className="form-group">
+                  <textarea
+                    className={
+                      errors.description
+                        ? " form-control form-control-lg border-danger"
+                        : "form-control form-control-lg"
+                    }
+                    placeholder="Project Description"
+                    name="description"
+                    value={this.state.description}
+                    onChange={this.onChange}
+                  />
+                  {errors.description && (
+                    <small className="text-danger">{errors.description}</small>
+                  )}
+                </div>
+                <h6>Start Date</h6>
+                <div className="form-group">
+                  <input
+                    type="date"
+                    className="form-control form-control-lg"
+                    name="start_date"
+                    value={this.state.start_date}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <h6>Estimated End Date</h6>
+                <div className="form-group">
+                  <input
+                    type="date"
+                    className="form-control form-control-lg"
+                    name="end_date"
+                    value={this.state.end_date}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <input
+                  type="submit"
+                  className="btn btn-primary btn-block mt-4"
+                />
+              </form>
             </div>
           </div>
         </div>
