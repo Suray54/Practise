c78584c20f16f4fd7f30e1cd80e4b2625137cfbe
Suranjan Rai <Suranjanthulungrai@gmail.com>
2021-07-18 19:51:02 +0545

    sidebar panel integrated

--- src/layouts/nav/nav.components.jsx
+++ src/layouts/nav/nav.components.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
 // import "./nav.js";
 import "./nav.styles.scss";
@@ -9,29 +9,30 @@ import { LINK } from "../../constant/link-constant";
 export class Navigation extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-md fixed-top">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
-            <img src={logo} alt="logo" />
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#collapsibleNavbar"
-          >
-            <i className="fa fa-bars" aria-hidden="true"></i>
-          </button>
-          <div className="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul className="navbar-nav  ml-auto">
-              {LINK.map((link, index) => (
-                <li key={index} className="nav-item">
-                  <Link className="nav-link" to={link.to}>
-                    {link.linkname}
-                  </Link>
-                </li>
-              ))}
-              {/* <li className="nav-item">
+      <>
+        <nav className="navbar navbar-expand-md fixed-top d-none d-sm-block">
+          <div className="container">
+            <Link className="navbar-brand" to="/">
+              <img src={logo} alt="logo" />
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#collapsibleNavbar"
+            >
+              <i className="fa fa-bars" aria-hidden="true"></i>
+            </button>
+            <div className="collapse navbar-collapse" id="collapsibleNavbar">
+              <ul className="navbar-nav  ml-auto">
+                {LINK.map((link, index) => (
+                  <li key={index} className="nav-item">
+                    <Link className="nav-link" to={link.to}>
+                      {link.linkname}
+                    </Link>
+                  </li>
+                ))}
+                {/* <li className="nav-item">
                 <Link className="nav-link" to="/">
                   Home
                 </Link>
@@ -41,12 +42,53 @@ export class Navigation extends Component {
                   Pokedex
                 </Link>
               </li> */}
-            </ul>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+        <DrawerMenu />
+      </>
     );
   }
 }
 
 export default Navigation;
+
+const DrawerMenu = (props) => {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-md fixed-top d-block d-sm-none">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+          <button onClick={showSidebar} className="burger-button">
+            <i className="fa fa-bars" aria-hidden="true"></i>
+          </button>
+        </div>
+      </nav>
+      <div className={sidebar ? "drawer-menu active" : "drawer-menu"}>
+        <ul>
+          {LINK.map((link, index) => (
+            <li key={index} className="drawer-menu_item" onClick={showSidebar}>
+              <Link className="drawer-menu_link" to={link.to}>
+                {link.linkname}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <i
+          className={
+            sidebar
+              ? "spin active fa fa-times-circle"
+              : "spin fa fa-times-circle"
+          }
+          onClick={showSidebar}
+        />
+      </div>
+    </>
+  );
+};
